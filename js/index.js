79d1e80814dae920f535877ee0fd311f841987b0
Adam Mathieson <mathieson1222@gmail.com>
2019-05-21 00:26:06 -0700

    adds eventListeners to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+const dkMode = document.querySelector('body')
+dkMode.style.background = "#36393d"
+
+const header = document.querySelector('header')
+header.style.background = "#36393d"
+
+const logo = document.querySelector('h1')
+logo.addEventListener('click', (e) => {
+    e.target.style.display = 'none'
+})
+
+const funBus = document.querySelector('header img')
+funBus.addEventListener('dblclick', () => {
+    logo.style.display = 'block'
+})
+
+const navItems = document.querySelectorAll('.nav-link')
+navItems.forEach(item => item.addEventListener('mouseover', (e) => {
+    e.target.style.color = '#ffff99'
+}))
+
+navItems.forEach(item => item.addEventListener('mouseout', (e) => {
+    e.target.style.color = '#212529'
+}))
+
+console.log(navItems)
