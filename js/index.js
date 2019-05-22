a6c1e9481fe81ff4206df28974a56aa29ea04f10
Adam Mathieson <mathieson1222@gmail.com>
2019-05-21 22:53:33 -0700

    adds a whole bunch of cool event listeners

--- js/index.js
+++ js/index.js
@@ -25,4 +25,46 @@ navItems.forEach(item => item.addEventListener('mouseout', (e) => {
     e.target.style.color = '#212529'
 }))
 
-console.log(navItems)
+
+// let mouseOver = document.querySelectorAll(".nav-link");
+// console.log(mouseOver)
+
+// mouseOver.forEach(item => item.addEventListener(
+//   "mouseover",
+//   function(event) {
+//     event.target.style.color = "red";
+//     setTimeout(function() {
+//       event.target.style.color = "";
+//     }, 250);
+//   }
+// ));
+
+
+const body = document.querySelector('body')
+body.addEventListener('mousemove', e => {
+    body.style.color = 'rgb('+e.offsetX+','+e.offsetY+', 40)';
+});
+
+const contentPick = document.querySelectorAll('.destination');
+console.log(contentPick);
+
+contentPick.forEach( item => item.addEventListener('click', event => {
+   console.log(`Work button fired a click event!`)
+//    event.stopPropagation();
+}));
+
+const content = document.querySelector('.content-pick')
+console.log(content)
+content.addEventListener('click', () => {
+    console.log('propagation')
+})
+
+const btn = document.querySelector('.btn')
+btn.onclick = function(event) {
+    event.target.style.color = 'red'
+    btn.style.background = 'green'
+    setTimeout(function() {
+        event.target.style.color = ''
+    }, 5000)
+}
+// console.log(btn)
